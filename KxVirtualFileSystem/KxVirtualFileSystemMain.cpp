c2628cd9a1afb1a3a2d3f3d88a5deb541ad073a2
KerberX <DaIvan97@gmail.com>
2019-01-03 01:55:46 +0300

    Test program sample updated

--- KxVirtualFileSystem/KxVirtualFileSystemMain.cpp
+++ KxVirtualFileSystem/KxVirtualFileSystemMain.cpp
@@ -25,20 +25,52 @@ int _tmain()
 	service->Install(L"C:\\Users\\Kerber\\Documents\\Visual Studio 2017\\Projects\\Kortex Mod Manager\\Kortex\\Bin\\Data\\VFS\\Drivers\\Win7 x64\\dokan2.sys");
 	service->Start();
 
-	//MirrorFS* mirror = new MirrorFS(service, L"C:\\Users\\Kerber\\Desktop\\Test", L"D:\\Game Files\\The Elder Scrolls\\Skyrim", 0, ULONG_MAX);
-	
-	MirrorFS* mirrorGC = new MirrorFS(service, L"C:\\Users\\Kerber\\Documents\\My Games\\Skyrim", L"D:\\Games\\Kortex Mod Manager\\Skyrim\\Default\\VirtualGameConfig");
-	MirrorFS* mirrorPL = new MirrorFS(service, L"C:\\Users\\Kerber\\AppData\\Local\\Skyrim", L"D:\\Games\\Kortex Mod Manager\\Skyrim\\Default\\PluginsOrder");
+	//ConvergenceFS* mainVFS = new ConvergenceFS(service, L"C:\\Users\\Kerber\\Desktop\\Test", L"C:\\Users\\Kerber\\Desktop\\TestWrite");
+	//mainVFS->AllowDeleteInVirtualFolder(true);
+	//mainVFS->AddVirtualFolder(L"D:\\Game Files\\The Elder Scrolls\\Skyrim");
+	//mainVFS->AddVirtualFolder(L"D:\\Games\\Kortex Mod Manager\\Skyrim\\Default\\Mods\\979ba932bb6d9e9b8049c2fda6fc255d\\ModFiles");
+	//mainVFS->AddVirtualFolder(L"D:\\Games\\Kortex Mod Manager\\Skyrim\\Default\\Mods\\7cacfd93a9caea44cacfd86e614cd348\\ModFiles");
 
-	ConvergenceFS* mainVFS = new ConvergenceFS(service, L"C:\\Users\\Kerber\\Desktop\\Test", L"C:\\Users\\Kerber\\Desktop\\TestWrite");
-	mainVFS->AllowDeleteInVirtualFolder(true);
-	mainVFS->AddVirtualFolder(L"D:\\Game Files\\The Elder Scrolls\\Skyrim");
-	//mainVFS->AddVirtualFolder(L"C:\\Users\\Kerber\\Desktop\\TestData");
-	mainVFS->AddVirtualFolder(L"D:\\Games\\Kortex Mod Manager\\Skyrim\\Default\\Mods\\979ba932bb6d9e9b8049c2fda6fc255d\\ModFiles");
-	mainVFS->AddVirtualFolder(L"D:\\Games\\Kortex Mod Manager\\Skyrim\\Default\\Mods\\7cacfd93a9caea44cacfd86e614cd348\\ModFiles");
-	//mirror->AddVirtualFolder(L"C:\\Users\\Kerber\\Desktop\\VF\\1");
-	//mirror->AddVirtualFolder(L"C:\\Users\\Kerber\\Desktop\\VF\\2");
-	//mirror->AddVirtualFolder(L"C:\\Users\\Kerber\\Desktop\\VF\\3");
+	MirrorFS* mainVFS = new MirrorFS(service, L"C:\\Users\\Kerber\\Desktop\\Test", L"D:\\Game Files\\The Elder Scrolls\\Skyrim");
+
+	wchar_t fileName[1024] = L"\"C:\\Users\\Kerber\\Desktop\\Test\\WolfTime x64.exe\"";
+	wchar_t workingDir[1024] = L"C:\\Users\\Kerber\\Desktop\\Test";
+
+	auto RunProcess = [&]()
+	{
+		STARTUPINFO startupInfo = {0};
+		startupInfo.cb = sizeof(startupInfo);
+
+		PROCESS_INFORMATION processInfo = {0};
+		processInfo.hProcess = INVALID_HANDLE_VALUE;
+		processInfo.hThread = INVALID_HANDLE_VALUE;
+
+		const DWORD options = 0;
+		const bool isOK = CreateProcessW(nullptr, fileName, nullptr, nullptr, FALSE, options, nullptr, workingDir, &startupInfo, &processInfo);
+		const DWORD error = GetLastError();
+
+		CloseHandle(processInfo.hThread);
+		CloseHandle(processInfo.hProcess);
+
+		fprintf(stdout, "CreateProcessW: %s with error code %u\r\n", isOK ? "true" : "false", error);
+	};
+	auto OpenFileDialog = [&]()
+	{
+		OPENFILENAMEW options = {0};
+		options.lStructSize = sizeof(options);
+
+		wchar_t buffer[1024] = {0};
+		options.lpstrFile = buffer;
+		options.nMaxFile = std::size(buffer);
+
+		options.lpstrInitialDir = workingDir;
+		options.Flags = OFN_DONTADDTORECENT|OFN_ENABLESIZING|OFN_NOCHANGEDIR;
+
+		GetOpenFileNameW(&options);
+		const DWORD error = GetLastError();
+		const DWORD error2 = CommDlgExtendedError();
+		return error;
+	};
 
 	char c = '\0';
 	while (c != 'x')
@@ -49,53 +81,17 @@ int _tmain()
 			case 'm':
 			{
 				int error = mainVFS->Mount();
-				//mirrorGC->Mount();
-				//mirrorPL->Mount();
 				fwprintf(stdout, L"%d: %s\r\n", error, MirrorFS::GetErrorCodeMessage(error).data());
-				break;
-			}
-			case 'c':
-			{
-				mainVFS->BuildDispatcherIndex();
 				break;
 			}
 			case 'r':
 			{
-				auto RunProcess = []()
-				{
-					STARTUPINFO startupInfo = {0};
-					startupInfo.cb = sizeof(startupInfo);
-
-					PROCESS_INFORMATION processInfo = {0};
-					processInfo.hProcess = INVALID_HANDLE_VALUE;
-					processInfo.hThread = INVALID_HANDLE_VALUE;
-
-					WCHAR s1[1024] = L"\"C:\\Users\\Kerber\\Desktop\\Test\\WolfTime x64.exe\"";
-					WCHAR s2[1024] = L"C:\\Users\\Kerber\\Desktop\\Test";
-
-					//MessageBoxW(nullptr, s1, s2, 0);
-					bool isOK = CreateProcessW(nullptr,
-											   s1,
-											   nullptr,
-											   nullptr,
-											   FALSE,
-											   0,//CREATE_DEFAULT_ERROR_MODE|CREATE_NO_WINDOW|DETACHED_PROCESS,
-											   nullptr,
-											   s2,
-											   &startupInfo,
-											   &processInfo
-					);
-					DWORD error = GetLastError();
-					//WaitForSingleObject(processInfo.hProcess, INFINITE);
-					CloseHandle(processInfo.hThread);
-					CloseHandle(processInfo.hProcess);
-
-					fprintf(stdout, "%s: %u\r\n", isOK ? "true" : "false", error);
-				};
-				std::thread t(RunProcess);
-				t.detach();
-				//RunProcess();
-
+				RunProcess();
+				break;
+			}
+			case 'o':
+			{
+				OpenFileDialog();
 				break;
 			}
 			case 's':
@@ -119,21 +115,14 @@ int _tmain()
 
 			case 'u':
 			{
-				fprintf(stderr, "%s\r\n", mainVFS->UnMount() ? "true" : "false");
-				mirrorGC->UnMount();
-				mirrorPL->UnMount();
+				mainVFS->UnMount();
 
+				fprintf(stderr, "%s\r\n", mainVFS->UnMount() ? "true" : "false");
 				break;
 			}
 			case 'x':
 			{
-				fprintf(stderr, "%s\r\n", mainVFS->UnMount() ? "true" : "false");
-				mirrorGC->UnMount();
-				mirrorPL->UnMount();
-
 				delete mainVFS;
-				delete mirrorGC;
-				delete mirrorPL;
 				delete service;
 				break;
 			}
