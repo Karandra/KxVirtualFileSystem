8b70ba05b948ac2e49a9ec238072adf7f83e4741
KerberX <DaIvan97@gmail.com>
2019-01-03 14:36:23 +0300

    Added checking if macros are already defined

--- KxVirtualFileSystem/IncludeWindows.h
+++ KxVirtualFileSystem/IncludeWindows.h
@@ -5,8 +5,13 @@ You should have received a copy of the GNU LGPL v3
 along with KxVirtualFileSystem. If not, see https://www.gnu.org/licenses/lgpl-3.0.html.
 */
 
+#ifndef WIN32_NO_STATUS
 #define WIN32_NO_STATUS 1
+#endif
+
+#ifndef NOMINMAX
 #define NOMINMAX 1
+#endif
 
 #include <Windows.h>
 #include "UndefWindows.h"
