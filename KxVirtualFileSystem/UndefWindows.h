9713c29b6c76395bcc7dba66bef3fd39d5069188
KerberX <DaIvan97@gmail.com>
2019-01-03 08:00:01 +0300

    Added separate file with '#undef's for macros wrom 'Windows.h'

--- KxVirtualFileSystem/UndefWindows.h
+++ KxVirtualFileSystem/UndefWindows.h
@@ -4,9 +4,8 @@ Copyright © 2018 Kerber. All rights reserved.
 You should have received a copy of the GNU LGPL v3
 along with KxVirtualFileSystem. If not, see https://www.gnu.org/licenses/lgpl-3.0.html.
 */
-
-#define WIN32_NO_STATUS 1
-#define NOMINMAX 1
-
-#include <Windows.h>
-#include "UndefWindows.h"
+#undef CreateService
+#undef StartService
+#undef GetFileAttributes
+#undef FormatMessage
+#undef GetMessage
