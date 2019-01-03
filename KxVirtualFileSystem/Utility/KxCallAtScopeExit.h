dc1debb222aa24a5278862073be63fc45189d8ec
KerberX <DaIvan97@gmail.com>
2019-01-03 08:00:49 +0300

    KxCallAtScopeExit updated with correct move semantics

--- KxVirtualFileSystem/Utility/KxCallAtScopeExit.h
+++ KxVirtualFileSystem/Utility/KxCallAtScopeExit.h
@@ -7,26 +7,51 @@ along with KxVirtualFileSystem. If not, see https://www.gnu.org/licenses/lgpl-3.
 #pragma once
 #include <utility>
 #include <functional>
+#include <optional>
 
 namespace KxVFS
 {
 	template<class T> class KxCallAtScopeExit
 	{
 		private:
-			T m_Functor;
+			std::optional<T> m_Functor;
 
 		public:
-			KxCallAtScopeExit(const T& functor)
-				:m_Functor(functor)
-			{
-			}
 			KxCallAtScopeExit(T&& functor)
 				:m_Functor(std::move(functor))
 			{
 			}
+			KxCallAtScopeExit(const T& functor)
+				:m_Functor(functor)
+			{
+			}
+			
+			KxCallAtScopeExit(KxCallAtScopeExit&& other)
+				:m_Functor(std::move(other.m_Functor))
+			{
+				other.m_Functor.reset();
+			}
+			KxCallAtScopeExit(const KxCallAtScopeExit& other)
+				:m_Functor(other.m_Functor)
+			{
+			}
+			
+			KxCallAtScopeExit& operator=(KxCallAtScopeExit&& other)
+			{
+				m_Functor = std::move(other.m_Functor);
+				other.m_Functor.reset();
+			}
+			KxCallAtScopeExit& operator=(const KxCallAtScopeExit& other)
+			{
+				:m_Functor = other.m_Functor;
+			}
+
 			~KxCallAtScopeExit()
 			{
-				(void)std::invoke(m_Functor);
+				if (m_Functor)
+				{
+					(void)std::invoke(*m_Functor);
+				}
 			}
 	};
 }
