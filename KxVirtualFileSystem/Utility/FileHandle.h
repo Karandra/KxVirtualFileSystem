f5a9cfb79ebb8b2715c9543b248205adc0ba9279
KerberX <DaIvan97@gmail.com>
2019-01-03 05:58:04 +0300

    FileHandle now uses HandleWrapper

--- KxVirtualFileSystem/Utility/FileHandle.h
+++ KxVirtualFileSystem/Utility/FileHandle.h
@@ -11,92 +11,26 @@ along with KxVirtualFileSystem. If not, see https://www.gnu.org/licenses/lgpl-3.
 
 namespace KxVFS
 {
-	class FileHandle final
+	enum class SeekMode
 	{
-		private:
-			HANDLE m_Handle = INVALID_HANDLE_VALUE;
+		Current = FILE_CURRENT,
+		Start = FILE_BEGIN,
+		End = FILE_END,
+	};
 
+	class FileHandle: public HandleWrapper<size_t, std::numeric_limits<size_t>::max()>
+	{
 		public:
-			FileHandle(HANDLE fileHandle = INVALID_HANDLE_VALUE)
-				:m_Handle(fileHandle)
+			FileHandle(THandle fileHandle = GetInvalidHandle())
+				:HandleWrapper(fileHandle)
 			{
 			}
 			FileHandle(FileHandle&& other)
+				:HandleWrapper(std::move(other))
 			{
-				*this = std::move(other);
-			}
-			FileHandle(const FileHandle&) = delete;
-			~FileHandle()
-			{
-				Close();
 			}
 			
-			FileHandle& operator=(const HANDLE& fileHandle)
-			{
-				return Assign(fileHandle);
-			}
-			FileHandle& operator=(FileHandle&& other)
-			{
-				Close();
-				m_Handle = other.Release();
-				return *this;
-			}
-			FileHandle& operator=(const FileHandle&) = delete;
-
 		public:
-			bool IsOK() const
-			{
-				return m_Handle != INVALID_HANDLE_VALUE;
-			}
-			HANDLE Get() const
-			{
-				return m_Handle;
-			}
-			FileHandle& Assign(const HANDLE& fileHandle)
-			{
-				Close();
-				m_Handle = fileHandle;
-				return *this;
-			}
-		
-			HANDLE Release()
-			{
-				HANDLE fileHandle = m_Handle;
-				m_Handle = INVALID_HANDLE_VALUE;
-				return fileHandle;
-			}
-			bool Close()
-			{
-				if (IsOK())
-				{
-					return ::CloseHandle(Release());
-				}
-				return false;
-			}
-		
-			bool SetDeleteOnClose(bool deleteOnClose);
-			int64_t GetSize() const
-			{
-				LARGE_INTEGER size = {0};
-				if (::GetFileSizeEx(m_Handle, &size))
-				{
-					return size.QuadPart;
-				}
-				return -1;
-			}
-			bool Seek(int64_t offset, DWORD mode = FILE_CURRENT)
-			{
-				LARGE_INTEGER pos = {0};
-				pos.QuadPart = offset;
-				return SetFilePointerEx(m_Handle, pos, &pos, mode);
-			}
-			int64_t GetPosition() const
-			{
-				LARGE_INTEGER pos = {0};
-				SetFilePointerEx(m_Handle, pos, &pos, FILE_CURRENT);
-				return pos.QuadPart;
-			}
-			KxDynamicStringW GetPath() const;
 			uint32_t GetAttributes() const
 			{
 				BY_HANDLE_FILE_INFORMATION info = {0};
@@ -106,29 +40,34 @@ namespace KxVFS
 				}
 				return INVALID_FILE_ATTRIBUTES;
 			}
-
-		public:
-			bool operator==(const FileHandle& other) const
+			int64_t GetFileSize() const
 			{
-				return m_Handle == other.m_Handle;
+				LARGE_INTEGER size = {0};
+				if (::GetFileSizeEx(m_Handle, &size))
+				{
+					return size.QuadPart;
+				}
+				return -1;
 			}
-			bool operator!=(const FileHandle& other) const
+			int64_t GetPosition() const
 			{
-				return !(*this == other);
+				LARGE_INTEGER pos = {0};
+				SetFilePointerEx(m_Handle, pos, &pos, FILE_CURRENT);
+				return pos.QuadPart;
 			}
-
-			explicit operator bool() const
+			
+			bool Seek(int64_t offset, DWORD mode = FILE_CURRENT)
 			{
-				return IsOK();
+				LARGE_INTEGER pos = {0};
+				pos.QuadPart = offset;
+				return SetFilePointerEx(m_Handle, pos, &pos, mode);
 			}
-			bool operator!() const
+			bool Seek(int64_t offset, SeekMode mode = SeekMode::Current)
 			{
-				return !IsOK();
+				return Seek(offset, static_cast<DWORD>(mode));
 			}
-	
-			operator HANDLE() const
-			{
-				return Get();
-			}
+			
+			bool SetDeleteOnClose(bool deleteOnClose);
+			KxDynamicStringW GetPath() const;
 	};
 }
