14ab82dc6d9227f4e0e985fcef971bcf88e6ba3b
KerberX <DaIvan97@gmail.com>
2019-02-22 19:14:29 +0300

    Added member variants of encoding conversion functions

--- KxVirtualFileSystem/Utility/KxDynamicString/KxBasicDynamicString.h
+++ KxVirtualFileSystem/Utility/KxDynamicString/KxBasicDynamicString.h
@@ -959,37 +959,53 @@ class KxBasicDynamicString
 
 		// Encoding conversion
 		template<class StringT = KxBasicDynamicString<wchar_t, t_StaticStorageLength, std::char_traits<wchar_t>, std::allocator<wchar_t>>>
-		static StringT to_utf16(const char* text, int length, int codePage)
+		static StringT to_utf16(const char* text, size_t length, int codePage)
 		{
 			StringT converted;
-			int lengthRequired = ::MultiByteToWideChar(codePage, 0, text, length, nullptr, 0);
-			if (lengthRequired != 0)
+			const int lengthRequired = ::MultiByteToWideChar(codePage, 0, text, static_cast<int>(length), nullptr, 0);
+			if (lengthRequired > 0)
 			{
-				converted.resize(static_cast<size_t>(lengthRequired + 1));
-				::MultiByteToWideChar(codePage, 0, text, length, converted.data(), lengthRequired);
-				converted.resize(static_cast<size_t>(lengthRequired - 1));
+				converted.resize(static_cast<size_t>(lengthRequired));
+				::MultiByteToWideChar(codePage, 0, text, static_cast<int>(length), converted.data(), lengthRequired);
 			}
 			return converted;
 		}
 
 		template<class StringT = KxBasicDynamicString<char, t_StaticStorageLength, std::char_traits<char>, std::allocator<char>>>
-		static StringT to_codepage(const wchar_t* text, int length, int codePage)
+		StringT to_utf16(int codePage) const
+		{
+			return to_utf16<StringT>(data(), length(), codePage);
+		}
+
+		template<class StringT = KxBasicDynamicString<char, t_StaticStorageLength, std::char_traits<char>, std::allocator<char>>>
+		StringT to_utf16() const
+		{
+			return to_utf16<StringT>(data(), length(), CP_UTF8);
+		}
+
+		template<class StringT = KxBasicDynamicString<char, t_StaticStorageLength, std::char_traits<char>, std::allocator<char>>>
+		static StringT to_codepage(const wchar_t* text, size_t length, int codePage)
 		{
 			StringT converted;
-			int lengthRequired = ::WideCharToMultiByte(codePage, 0, text, length, nullptr, 0, nullptr, nullptr);
-			if (lengthRequired != 0)
+			const int lengthRequired = ::WideCharToMultiByte(codePage, 0, text, static_cast<int>(length), nullptr, 0, nullptr, nullptr);
+			if (lengthRequired > 0)
 			{
-				converted.resize(static_cast<size_t>(lengthRequired + 1));
-				::WideCharToMultiByte(codePage, 0, text, length, converted.data(), lengthRequired, nullptr, nullptr);
-				converted.resize(static_cast<size_t>(lengthRequired - 1));
+				converted.resize(static_cast<size_t>(lengthRequired));
+				::WideCharToMultiByte(codePage, 0, text, static_cast<int>(length), converted.data(), lengthRequired, nullptr, nullptr);
 			}
 			return converted;
 		}
 
 		template<class StringT = KxBasicDynamicString<char, t_StaticStorageLength, std::char_traits<char>, std::allocator<char>>>
-		static StringT to_utf8(const wchar_t* text, int length, int codePage)
+		StringT to_codepage(int codePage) const
 		{
-			return to_codepage(text, length, CP_UTF8);
+			return to_codepage<StringT>(data(), length(), codePage);
+		}
+
+		template<class StringT = KxBasicDynamicString<char, t_StaticStorageLength, std::char_traits<char>, std::allocator<char>>>
+		StringT to_utf8() const
+		{
+			return to_codepage<StringT>(data(), length(), CP_UTF8);
 		}
 
 		// Formatting
