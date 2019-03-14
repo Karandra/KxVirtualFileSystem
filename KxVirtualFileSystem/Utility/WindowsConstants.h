3f593b57d69b03d34e7d28b0190d85a9898f6d0e
KerberX <DaIvan97@gmail.com>
2019-03-14 03:14:56 +0300

    Fixed date in header

--- KxVirtualFileSystem/Utility/WindowsConstants.h
+++ KxVirtualFileSystem/Utility/WindowsConstants.h
@@ -1,5 +1,5 @@
 /*
-Copyright © 2018 Kerber. All rights reserved.
+Copyright © 2019 Kerber. All rights reserved.
 
 You should have received a copy of the GNU LGPL v3
 along with KxVirtualFileSystem. If not, see https://www.gnu.org/licenses/lgpl-3.0.html.
